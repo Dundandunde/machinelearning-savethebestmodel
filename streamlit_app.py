5c7ee47c3d5cc948f09a3a16b04734a790caa73b
Dundandunde <dungnnp22413@st.uel.edu.vn>
2024-11-11 22:06:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ st.title('🎈 Learning')
 st.write('Hello world!')
 
 uploaded_file = st.file_uploader("/content/Womens Clothing E-Commerce Reviews.csv", type=["csv"])
+if uploaded_file is not None:
+    # Đọc tệp CSV bằng pandas
+    df = pd.read_csv(uploaded_file)
+    
+    # Hiển thị dữ liệu dưới dạng bảng
+    st.write(df)
