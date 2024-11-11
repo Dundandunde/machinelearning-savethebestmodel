a0fa0082f77100d82b4da246472be83ba9a90075
Dundandunde <dungnnp22413@st.uel.edu.vn>
2024-11-11 22:21:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,4 @@ uploaded_file = st.file_uploader("Chọn tệp CSV", type=["csv"])
 if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         st.write("Dữ liệu đã tải lên:")
+        df
