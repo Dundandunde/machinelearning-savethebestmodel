e04432ddf6e64a2ec52cbab4072e3ae8e84c19e1
Dundandunde <dungnnp22413@st.uel.edu.vn>
2024-11-11 22:53:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,13 @@ with st.expander('Data'):
     # Bước 2: Tiền xử lý dữ liệu
 with st.expander('Tiền xử lý dữ liệu'):
         if 'text' in df.columns and 'sentiment' in df.columns:
+            # Vector hóa dữ liệu văn bản
             vectorizer = TfidfVectorizer(stop_words='english')
             X = vectorizer.fit_transform(df['text'])
             y = df['sentiment']
-           X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            
+            # Bước 3: Chia dữ liệu thành tập huấn luyện và kiểm tra
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             
             # Bước 4: Huấn luyện mô hình Logistic Regression
             model = BernoulliNB()
