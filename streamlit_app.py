c12c4c27a1722f2fe44806b8db82f1921d7d9513
Dundandunde <dungnnp22413@st.uel.edu.vn>
2024-11-11 21:51:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-
 st.title('🎈 Machine Learning')
 st.write('Hello world!')
+
+url = 'https://drive.google.com/file/d/16uecFM3ENg5sfdCBNk8xqcX79j3CVKiX/view?usp=drive_link'
+url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+data = pd.read_csv(url, encoding='unicode_escape')
+data.head(10)
 
 def load_model(model_name):
     if model_name == 'Logistic Regression':
