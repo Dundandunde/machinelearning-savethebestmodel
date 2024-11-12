8ab41cbe351a5a2e79a665452dc0111ed6561080
Dundandunde <dungnnp22413@st.uel.edu.vn>
2024-11-12 14:52:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ with st.expander('Analyze CSV'):
 
 #
     if upl:
-        df = pd.read_excel(upl)
+        df = pd.read_csv(upl)
         del df['Unnamed: 0']
         df['score'] = df['tweets'].apply(score)
         df['analysis'] = df['score'].apply(analyze)
