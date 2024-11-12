31b3a82512e0df5634f88f751d61e02e07abc38f
Dundandunde <dungnnp22413@st.uel.edu.vn>
2024-11-12 14:45:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,10 @@ with st.expander('Analyze Text'):
     text = st.text_input('Text here: ')
     if text:
         blob = TextBlob(text)
-        st.write('Tích cực: ', round(blob.sentiment.polarity,2))
-        st.write('Tiêu cực: ', round(blob.sentiment.subjectivity,2))
+        st.write('Điểm tích cực: ', round(blob.sentiment.polarity,2))
+        st.write('Điểm chủ quan: ', round(blob.sentiment.subjectivity,2))
+
+    pre = st.text_input('Clean Text: ')
+    if pre:
+        st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True ,
+                                 stopwords=True ,lowercase=True ,numbers=True , punct=True))
