7009f1f4a4d5e4e0ae8eef5d5972e751a0c19914
Dundandunde <dungnnp22413@st.uel.edu.vn>
2024-11-12 14:21:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,27 +6,16 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import joblib
+from textblob import TextBlob
+import cleantext 
 st.title("Ứng Dụng Phân Tích Cảm Xúc")
-st.write('Hello world!')
 
 # Sử dụng file_uploader để tải tệp CSV
-with st.expander('Data'):
-        uploaded_file = st.file_uploader("Chọn tệp CSV", type=["csv"])
-        if uploaded_file is not None:
-                df = pd.read_csv(uploaded_file)
-                st.write("Dữ liệu đã tải lên:")
-                df
-                
-                st.write('**Biến độc lập X**')
-                X = df.drop(columns=["Rating"])
-                X
-                st.write('**Biến phụ thuộc Y**')
-                Y = df.Rating
-                Y
+with st.expander('Analyze Text'):
+        text = st.text_input ('Text here: ')
+        if text: 
+                blob = TextBlob(text) 
+                st.write('Tích cực: ', round(blob.sentiment.polarity, 2))
+                st.write('Tiêu cực: ', round(blob.sentiment.subjectivity, 2))
 
-with st.expander('Tiền xử lý dữ liệu'):
-        df.rename(columns={"Review Text": "review", "Rating" : "rating"}, inplace = True)
-        df['review'] = df['review'].astype(str)  # Chuyển tất cả các giá trị thành chuỗi
-        df = df.dropna(subset=['review'])  # Xóa các hàng có giá trị NaN trong cột 'review'
-        df['review'] = df['review'].apply(clean_text)  # Loại bỏ ký tự đặc biệt
-        st.write("Các cột trong dữ liệu: ", df.columns)
+
